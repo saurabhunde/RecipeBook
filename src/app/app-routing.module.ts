843f55b2b243f5f5f68fd22dacfc9824b06b5db6
saurabh-unde <saurabhu@cybage.com>
2018-03-30 19:28:24 +0530

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/Router'
+
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes : Routes =[
+    {path:'recipes',component:RecipesComponent},
+    {path:'shopping_list', component:ShoppingListComponent},
+    {path:'',redirectTo:'/recipes',pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
